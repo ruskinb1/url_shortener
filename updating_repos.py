14dfda6ef6e336f3c624684e44864361f0fb5b63
Ruskin Singh <66462896+ruskinb1@users.noreply.github.com>
2024-05-26 18:05:00 +0530

    Create updating_repos.py

---
+++ updating_repos.py
@@ -0,0 +1,49 @@
+from github import Auth
+from github import Github
+import argparse
+import os
+
+def create_branch(target_repo, source_branch, target_branch):
+    repo = g.get_repo(target_repo)
+    sb = repo.get_branch(source_branch)
+    repo.create_git_ref(ref=f'refs/heads/{target_branch}', sha=sb.commit.sha)
+
+
+def create_file(target_repo, target_branch, source_file):
+    repo = g.get_repo(target_repo)
+    with open(source_file) as file:
+        data = file.read()
+    repo.create_file("source_file", f"{source_file} created", data, branch=target_branch)
+
+
+def create_pr(target_repo, source_branch, target_branch, title, body):
+    repo = g.get_repo(target_repo)
+    pr = repo.create_pull(base=source_branch, head=target_branch, title= title, body=body)
+
+
+def main(args):
+    ''' Method to getting the arguments and calling the copy_module function'''
+    token = os.environ['SECRET_TOKEN']
+    g = Github(token)
+    parser = argparse.ArgumentParser(description='File arguments for file creation in repos')
+    parser.add_argument("--source_repo", help="source_repo")
+    parser.add_argument("--target_branch", help="target_branch")
+    parser.add_argument("--source_file", help="source_file")
+    args = parser.parse_args()
+    title = "Pr Title workflow Added "
+    body = '''
+    Added the github workflow for checking the PR Title
+    '''
+    with open('target_repos.json') as f:
+        target_repo_list = json.load(f)
+        
+    for target_repo in target_repos_list:
+        target_repo_name = target_repo["name"]
+        target_repo_default_branch_name = target_repo["defaultBranchRef"]["name"]
+        
+        create_branch(target_repo_name, target_repo_default_branch_name, args.target_branch)
+        create_file(target_repo_name, args.target_branch, args.source_file)
+        create_pr(target_repo_name, target_repo_default_branch_name, args.target_branch, title, body)
+
+if __name__ == "__main__":
+    main(sys.argv)
