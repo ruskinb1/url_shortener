9f16cdef581ebbd572ba90b5b3f7ec3074743d6a
Ruskin Singh <66462896+ruskinb1@users.noreply.github.com>
2025-07-30 17:37:44 +0530

    Update updating_repos.py

--- updating_repos.py
+++ updating_repos.py
@@ -3,7 +3,7 @@ from github import Github
 import argparse
 import os
 import sys
-import json
+
 
 def create_branch(target_repo_name, source_branch, target_branch):
     repo = g.get_repo(target_repo_name)
